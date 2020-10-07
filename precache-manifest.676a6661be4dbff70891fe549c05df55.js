fd441600e300aa97457730ace0f8a34baa51464c
Nicholas Montgomery <montgomerynicholas30@gmail.com>
2020-10-06 23:35:09 -0700

    Updates

---
+++ precache-manifest.676a6661be4dbff70891fe549c05df55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021ae1bd43117cf5f4d807b1f5038588",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "7bbd75f33bf6f6aae370",
+    "url": "/my-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5ab1e35ec4797cb51f1a",
+    "url": "/my-app/static/js/2.068a67b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.068a67b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bbd75f33bf6f6aae370",
+    "url": "/my-app/static/js/main.6a25c653.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
